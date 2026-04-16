f7dc440d526a4f2961ab6eabe96a9570bb12968e
Alitarek3102005 <tali19985@gmail.com>
2026-04-17 00:18:30 +0200

    adding the eureka server and connect all the microservices to it for communicating to each other

--- Catalog-Service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ Catalog-Service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
--- Payment-Service/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ Payment-Service/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
