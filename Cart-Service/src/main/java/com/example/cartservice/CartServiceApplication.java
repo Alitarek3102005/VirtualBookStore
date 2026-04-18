9e26cff95dba7a145c851bfeaf4ebaf1ca2a1186
Zako10 <01121897572amr@gmail.com>
2026-04-18 14:19:04 +0200

    Update CartServiceApplication.java

--- Cart-Service/src/main/java/com/example/cartservice/CartServiceApplication.java
+++ Cart-Service/src/main/java/com/example/cartservice/CartServiceApplication.java
@@ -2,6 +2,8 @@ package com.example.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
