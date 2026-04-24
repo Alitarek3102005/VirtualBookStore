88438930451c40836418830666ad697556a99f00
Abda11ah255 <adhamabdallah348@gmail.com>
2026-04-24 23:06:08 +0300

    final version(Iwish)

--- Authentication-Service/src/main/java/com/example/authenticationservice/Service/UserServiceImpl.java
+++ Authentication-Service/src/main/java/com/example/authenticationservice/Service/UserServiceImpl.java
@@ -4,13 +4,16 @@ import com.example.authenticationservice.Dto.AuthResponse;
 import com.example.authenticationservice.Dto.LoginRequest;
 import com.example.authenticationservice.Dto.RegisterRequest;
 import com.example.authenticationservice.Dto.UserMapper;
+import com.example.authenticationservice.Entity.Role;
 import com.example.authenticationservice.Entity.User;
 import com.example.authenticationservice.Repository.UserRepository;
 import com.example.authenticationservice.Security_Config.JwtUtil;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,28 +27,28 @@ public class UserServiceImpl implements UserService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwt;
 
+    @Transactional
     @Override
+
     public AuthResponse register(RegisterRequest request) {
 
-        // check username
-     if (userRepository.findByUsername(request.getUsername()).isPresent()){
-         throw new  RuntimeException("Username already exists");
-     }
-        // check email
-    if(userRepository.findByEmail(request.getEmail()).isPresent()){
-        throw new RuntimeException("Email already exists");
-    }
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
 
-        // map DTO -> Entity
-    User user =userMapper.mapToUser(request);
-        // hash password
-    user.setPassword(passwordEncoder.encode(user.getPassword()));
-        // default values
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = userMapper.mapToUser(request);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        user.setRole(Role.USER);
+
         user.setEnabled(true);
 
-        // save
-    User savedUser=userRepository.save(user);
-        String token = jwt.generateToken(savedUser.getUsername(), String.valueOf(savedUser.getRole()));
+        User savedUser = userRepository.save(user);
 
         return AuthResponse.builder()
                 .id(savedUser.getId())
@@ -53,10 +56,8 @@ public class UserServiceImpl implements UserService {
                 .email(savedUser.getEmail())
                 .fullName(savedUser.getFullName())
                 .role(savedUser.getRole().name())
-                .token(token)
                 .build();
     }
-
     @Override
     public AuthResponse login(LoginRequest request) {
 
@@ -67,26 +68,22 @@ public class UserServiceImpl implements UserService {
                 )
         );
 
-        if (authentication.isAuthenticated()) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
-            User user = userRepository.findByUsername(request.getUsername())
-                    .orElseThrow();
+        User user = userRepository.findByUsername(userDetails.getUsername())
+                .orElseThrow();
 
-            String token = jwt.generateToken(
-                    user.getUsername(),
-                    user.getRole().name()
-            );
+        String token = jwt.generateToken(
+                user.getUsername(),
+                user.getRole().name()
+        );
 
-            return AuthResponse.builder()
-                    .id(user.getId())
-                    .username(user.getUsername())
-                    .email(user.getEmail())
-                    .fullName(user.getFullName())
-                    .role(user.getRole().name())
-
-                    .token(token)
-                    .build();
-        }
-
-        throw new RuntimeException("Invalid username or password");
+        return AuthResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .fullName(user.getFullName())
+                .role(user.getRole().name())
+                .token(token)
+                .build();
     }}
